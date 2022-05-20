bbe0f02036dbed9a873697096c4f691c89d08f70
nikkirosche <101503635+nikkirosche@users.noreply.github.com>
2022-05-20 23:51:16 +0800

    updated commit

--- server.js
+++ server.js
@@ -32,70 +32,85 @@ const pool = new Pool(pgConnectionConfigs);
 //===============================================LOGIN & COOKIE BACKEND=========================================================//
 //for new user
 const newUser = (req, res) => {
-    const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8'});
-    shaObj.update(req.body.password);
-    const hashedPassword = shaObj.getHash('HEX');
+  const shaObj = new jsSHA("SHA-512", "TEXT", { encoding: "UTF8" });
+  shaObj.update(req.body.password);
+  const hashedPassword = shaObj.getHash("HEX");
 
-    //store hashed password in DB
-    const values = [
-        req.body.first_name,
-        req.body.last_name,
-        req.body.display_name,
-        req.body.email,
-        hashedPassword
-    ];
-    console.log(req.body)
-    //make query to DB
-    pool.query(`INSERT INTO users (first_name, last_name, display_name, email, password) VALUES ($1, $2, $3, $4, $5)`, values,
+  //store hashed password in DB
+  const values = [
+    req.body.first_name,
+    req.body.last_name,
+    req.body.display_name,
+    req.body.email,
+    hashedPassword,
+  ];
+  //check if email exist
+//   const emailAvail = pool.query(`SELECT email FROM users`, (req, res) => {
+//     if (req.body.email === true) {
+//       res.send(
+//         alert("Email already exist, try another email!")
+//       );
+//       return;
+//     }
+//   });
+  //make query to DB
+  pool.query(
+    `INSERT INTO users (first_name, last_name, display_name, email, password) VALUES ($1, $2, $3, $4, $5)`,
+    values,
     (err, result) => {
-        if (err) {
-            console.log('Error detected', err);
-            res.status(404).send(err);
-            return;
-        }
-        res.redirect('/login');
-    })
+      if (err) {
+        console.log("Error detected", err);
+        res.status(404).send(err);
+        return;
+      }
+      res.redirect("/login");
+    }
+  );
 };
 
 //for login
 const authUser = (req, res) => {
-    const values = req.body.email;
-    pool.query(`SELECT * FROM users WHERE email = ${req.body.email}`, (err, result) =>{
-        if (err) {
-            console.log('err executing query');
-            res.status(404).send(result.rows);
-            return;
-        }
-        //did not find user with that email
-        if (result.rows.length === 0) {
-            res.status(404).send('Login failed');
-            return;
-        }
-        //get user record from results
-        const user = result.rows[0];
-        const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8'});
-        //input password from the request to the SHA object
-        shaObj.update(req.body.password);
-        //get hashedPassword
-        const hashedPassword = shaObj.getHash('HEX');
+  const values = [req.body.email];
+  pool.query(`SELECT * FROM users WHERE email = $1`, values, (err, result) => {
+    if (err) {
+      console.log("err executing query");
+      console.log(err);
+      res.status(404).send(err);
+      return;
+    }
+    //did not find user with that email
+    console.log(result.rows);
+    if (result.rows.length === 0) {
+      res.status(404).send("Login failed part1");
+      return;
+    }
+    //get user record from results
+    const user = result.rows[2];
+    const shaObj = new jsSHA("SHA-512", "TEXT", { encoding: "UTF8" });
+    //input password from the request to the SHA object
+    shaObj.update(req.body.password);
+    //get hashedPassword
+    const hashedPassword = shaObj.getHash("HEX");
 
-        // to tally the password in database with above hashed one
-        if (user.password !== hashedPassword) {
-            res.status(404).send('Login failed');
-            return;
-        }
-        // password hashmatches and we authenticate user
-        // for cookie
-        res.cookie('loggedIn', true);
-        // redirect to home page
-        res.redirect('/home');
-    })
+    // to tally the password in database with above hashed one
+    console.log(hashedPassword);
+    if (user.password !== hashedPassword) {
+      res.status(404).send("Login failed");
+      return;
+    }
+    // password hashmatches and we authenticate user
+    // for cookie
+    res.cookie("loggedIn", true);
+    // redirect to home page
+    res.redirect("/home");
+  });
+};
+
+//===============================================JOURNAL ENTRY SECTION===========================================================//
+//entry POST request
+const journalLog = (req, res) => {
+
 }
-
-
-
-//===============================================CALENDAR SECTION===========================================================//
-
 
 //===============================================CONNECT TO EJS FILE========================================================//
 //connect to intro page
@@ -123,6 +138,9 @@ app.post("/register", newUser);
 app.get("/home", (req, res) => {
   res.render("home");
 });
+
+//link journal entry to home page for posting
+app.post("/home", journalLog);
 
 //connect to entry log page
 app.get("/entries", (req, res) => {
